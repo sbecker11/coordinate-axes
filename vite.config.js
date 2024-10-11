cf5951689b7216e1510d3bbb3728be25b0b26f20
Shawn Becker <sbecker11@me.com>
2024-10-10 22:10:10 -0600

    Now working at https://sbecker11.github.io/coordinate-axes/

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/coordinate-axes/', // Set the base URL for GitHub Pages
   build: {
     outDir: 'dist',
     rollupOptions: {
-      external: ['three'],
-      output: {
-        globals: {
-          three: 'THREE'
-        }
-      }
     }
   },
   resolve: {
