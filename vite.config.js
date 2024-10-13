c4f97ff4e30df15d75967aff02455871a664d12d
Shawn Becker <sbecker11@me.com>
2024-10-12 18:59:16 -0600

    removed unused vue and react additions

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   base: '/coordinate-axes/', // Set the base URL for GitHub Pages
   build: {
     outDir: 'dist',
     rollupOptions: {
+      // Add any Rollup options here if needed
     }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src')
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'] // Added extensions for JS, JSX, TS, TSX, and Vue
-  },
-  esbuild: {
-    jsxInject: `import React from 'react'` // Added JSX inject for React
-  },
-  plugins: [vue()] // Added Vite plugin for Vue
+  }
 });
